70e224a6bd7943fd704c60e729c61778be0516fc
Stephen <stephengunn@gmail.com>
2023-08-27 11:09:06 -0500

    basic setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import '$styles/vars.css';
+	import '$styles/app.css';
+</script>
+
+<header>
+	<a href="/">
+		<div class="title">Conway's Game Of Life</div>
+		<div class="subtitle">In Svelte (kit)</div>
+	</a>
+</header>
+<aside />
+<slot />
+
+<style>
+	header {
+		padding: 0.5rem;
+		border-bottom: 1px solid var(--accent);
+	}
+
+	a {
+		text-decoration: none;
+	}
+
+	.title {
+		font-size: 1.2rem;
+	}
+	.subtitle {
+		color: var(--secondary);
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>Conway's Game Of Life in Svelte</title>
+</svelte:head>
+
+<style>
+</style>
